55365c1488ddb6c1a6e099ccc679deb6975c1c3e
kyusov <37961322+kyusov@users.noreply.github.com>
2019-10-13 21:00:29 +0800

    First commit, i hope this work :)

---
+++ precache-manifest.366905ef783663046b0f65b86cac29f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17e2cff94e58869fecc26ab097d72d95",
+    "url": "/decen/index.html"
+  },
+  {
+    "revision": "f7001a39bc51055b1d47",
+    "url": "/decen/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "53b9ffa6c8f427e90d91",
+    "url": "/decen/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f7001a39bc51055b1d47",
+    "url": "/decen/static/js/2.7f352f2c.chunk.js"
+  },
+  {
+    "revision": "53b9ffa6c8f427e90d91",
+    "url": "/decen/static/js/main.0a5741d3.chunk.js"
+  },
+  {
+    "revision": "3d0298b5876f5baafd9d",
+    "url": "/decen/static/js/runtime-main.3d9b832e.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/decen/static/media/persik.4e1ec840.png"
+  }
+]);
